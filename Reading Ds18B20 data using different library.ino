bb53038b78bc90bf4efc3296903d3151bdb0da9e
rid47 <ridwanmizan@gmail.com>
2017-10-10 18:09:02 +0600

    Add files via upload

---
+++ Reading Ds18B20 data using different library.ino
@@ -0,0 +1,132 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <Streaming.h>
+
+#define SLEEP_DELAY_IN_SECONDS  30
+#define ONE_WIRE_BUS            D4      // DS18B20 pin
+
+const char* ssid = "DataSoft_WiFi";
+const char* password = "support123";
+
+const char* mqtt_server = "iot.eclipse.org";
+//const char* mqtt_username = "<MQTT_BROKER_USERNAME>";
+//const char* mqtt_password = "<MQTT_BROKER_PASSWORD>";
+const char* mqtt_topic = "sensors/test/temperature";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature DS18B20(&oneWire);
+
+char temperatureString[6];
+
+void setup() {
+  // setup serial port
+  Serial.begin(115200);
+
+  // setup WiFi
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  // setup OneWire bus
+  DS18B20.begin();
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) 
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    //if your MQTT broker has clientID,username and password
+    //please change following line to    if (client.connect(clientId,userName,passWord))
+    if (client.connect(clientId.c_str()))
+    {
+      Serial.println("connected");
+     //once connected to MQTT broker, subscribe command if any
+      //client.subscribe("OsoyooCommand");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 6 seconds before retrying
+      delay(6000);
+    }
+  }
+} //end reconnect()
+
+float getTemperature() {
+  Serial << "Requesting DS18B20 temperature..." << endl;
+  float temp;
+  do {
+    DS18B20.requestTemperatures(); 
+    temp = DS18B20.getTempCByIndex(0);
+    delay(100);
+  } while (temp == 85.0 || temp == (-127.0));
+  return temp;
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  float temperature = getTemperature();
+  // convert temperature to a string with two digits before the comma and 2 digits for precision
+  dtostrf(temperature, 2, 2, temperatureString);
+  // send temperature to the serial console
+  Serial << "Sending temperature: " << temperatureString << endl;
+  // send temperature to the MQTT topic
+  client.publish(mqtt_topic, temperatureString);
+
+  Serial << "Closing MQTT connection..." << endl;
+  client.disconnect();
+
+  Serial << "Closing WiFi connection..." << endl;
+  WiFi.disconnect();
+
+  delay(100);
+
+  Serial << "Entering deep sleep mode for " << SLEEP_DELAY_IN_SECONDS << " seconds..." << endl;
+  ESP.deepSleep(SLEEP_DELAY_IN_SECONDS * 1000000, WAKE_RF_DEFAULT);
+  //ESP.deepSleep(10 * 1000, WAKE_NO_RFCAL);
+  delay(500);   // wait for deep sleep to happen
+}
